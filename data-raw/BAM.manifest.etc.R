9a2d883aab16ae639cc2486b7e6de2dc52028fc9
Steven G. Rozen <steve@steverozen.net>
2021-06-14 16:00:28 +0800

    created code to generate (essentiall) aliquot id, normal bam ID, tumor bam ID triples, and generated the dripples (for collaboratory)

--- data-raw/BAM.manifest.etc.R
+++ data-raw/BAM.manifest.etc.R
@@ -1,6 +1,5 @@
-# Join info on BAM file object ids with SP IDs
+# For info on downloaded data see notes-on-icgc.txt
 
-samp.sheet <- data.table::fread("data-raw/pcawg_sample_sheet.v1.4.2016-09-14.tsv")
 
 bam.info <- data.table::fread("data-raw/collaboratory_WGS_BAMS_repository_1623246282.tsv")
 
@@ -9,7 +8,7 @@ stopifnot(unique(bam.info$format) == "BAM")
 
 
 bam.info <- bam.info[  ,
-                       c("Object ID", "ICGC Donor", "Specimen ID", "Specimen Type",
+                       c("Object ID", "File Name", "ICGC Donor", "Specimen ID", "Specimen Type",
                          "Size (bytes)")]
 
 si <- split(bam.info, f = bam.info$`ICGC Donor`)
@@ -35,6 +34,52 @@ organize.one.donor <- function(ssi) {
   return(max.specs)
 }
 
-debug(organize.one.donor)
+# debug(organize.one.donor)
 
-foo <- lapply(si, organize.one.donor)
+donor.pairs <- lapply(si, organize.one.donor)
+
+is.normal <- function(one.specimen) {
+  return(grepl("Normal", one.specimen$`Specimen Type`))
+}
+
+flatten.one.donor <- function(pair) {
+  norm.index <- which(unlist(lapply(pair, is.normal)))
+  if (length(norm.index) == 0) {
+    browser()
+    stop("No normal sample")
+  }
+  pp <- pair[-norm.index]
+  ppp <- lapply(pp, merge.tumor.and.normal, pair[[norm.index]])
+  ppp <- do.call(rbind, ppp)
+  return(ppp)
+}
+
+merge.tumor.and.normal <- function(tumor.row, normal.row) {
+  colnames(tumor.row) <- paste0("T_", colnames(tumor.row))
+  colnames(normal.row) <- paste0("N_", colnames(normal.row))
+  rr <- cbind(tumor.row, normal.row)
+  return(rr)
+}
+
+# For debugging:
+which(unlist(lapply(donor.pairs, length)) > 2)
+# 302, 631, ...
+
+no.match <- which(unlist(lapply(donor.pairs, length)) == 1)
+
+donor.pairs <- donor.pairs[-no.match]
+
+tumor.normal.pair <- tibble::as_tibble(do.call(rbind, lapply(donor.pairs, flatten.one.donor)))
+
+samp.sheet <- data.table::fread("data-raw/pcawg_sample_sheet.v1.4.2016-09-14.tsv")
+
+samp.sheet <- samp.sheet[samp.sheet$library_strategy == "WGS", ]
+
+jj <- dplyr::inner_join(samp.sheet, tumor.normal.pair, by  = c("icgc_specimen_id" = "N_Specimen ID"))
+
+data.table::fwrite(jj, "collaboratory_bams_full.csv")
+
+## Simplify the columns
+jjj <- jj[ , c("aliquot_id", "icgc_donor_id", "icgc_specimen_id", "T_Object ID", "T_File Name", "N_Object ID", "N_File Name")]
+
+data.table::fwrite(jj, "collaboratory_bams.csv")
