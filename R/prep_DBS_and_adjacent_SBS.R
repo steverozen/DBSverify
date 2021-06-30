18b5ec2fcccf264836061e1058f1532acb1727b1
Steven G. Rozen <steve@steverozen.net>
2021-06-30 17:32:58 +0800

    Tested on one input VCF

--- R/prep_DBS_and_adjacent_SBS.R
+++ R/prep_DBS_and_adjacent_SBS.R
@@ -1,33 +1,44 @@
-prep_DBS_and_adjacent_SBS <- function(vcf.name) {
-  # vcfs <- ICAMS::ReadAndSplitMutectVCFs(vcf.name)
-  vcfs <- ICAMS::ReadAndSplitVCFs(vcf.name,
-                                  caller = "unknown")
-  dbs.vcf <- vcfs$DBS[[1]]
-  sbs.vcf <- vcfs$SBS[[1]]
+# get DBS and try to merge adjacent SBSs.  This has only been tested on the Hartwig data.
 
-  sbs.vcf.file <- tempfile()
-  colnames(sbs.vcf)[1] <- "#CHROM"
-  sbs.vcf <- remove_named_column(sbs.vcf, "VAF")
+prep_DBS_and_adjacent_SBS <-
+  function(vcf.name,
+           remove.from.dbs = c("QUAL", "INFO", "FORMAT", "VAF", "read.depth"),
+           remove.from.sbs = remove.from.dbs) {
+    # vcfs <- ICAMS::ReadAndSplitMutectVCFs(vcf.name)
+    vcfs <- ICAMS::ReadAndSplitVCFs(vcf.name,
+                                    caller = "unknown")
+    dbs.vcf <- vcfs$DBS[[1]]
+    dbs.vcf <- remove_named_column(dbs.vcf, remove.from.dbs)
 
-  data.table::fwrite(data.table::as.data.table(sbs.vcf),
-                     sbs.vcf.file,
-                     sep       = "\t",
-                     col.names = TRUE,
-                     scipen    = 10000000) # Don't use scientific notation
+    sbs.vcf <- vcfs$SBS[[1]]
 
-  new.split <- ICAMS::ReadAndSplitVCFs(files           = sbs.vcf.file,
-                                       variant.caller  = "unknown",
-                                       always.merge.SBS = TRUE)
+    sbs.vcf.file <- tempfile()
+    colnames(sbs.vcf)[1] <- "#CHROM"
+    sbs.vcf <- remove_named_column(sbs.vcf, remove.from.sbs)
 
-  new.dbs <- new.split$DBS[[1]]
-  new.dbs <- remove_named_column(new.dbs, "VAF")
+    data.table::fwrite(data.table::as.data.table(sbs.vcf),
+                       sbs.vcf.file,
+                       sep       = "\t",
+                       col.names = TRUE,
+                       scipen    = 10000000) # Don't use scientific notation
 
-  colnames(new.dbs)[3:ncol(new.dbs)] <-
-    paste(colnames(new.dbs)[3:ncol(new.dbs)], "ad", sep = "_")
+    new.split <- ICAMS::ReadAndSplitVCFs(files           = sbs.vcf.file,
+                                         variant.caller  = "unknown",
+                                         always.merge.SBS = TRUE)
 
-  j1 <- dplyr::full_join(dbs.vcf, new.dbs)
+    new.dbs <- new.split$DBS[[1]]
+    new.dbs <- remove_named_column(new.dbs,
+                                   c("VAF", "read.depth", "remark.for.DBS"))
 
-  return(0)
+    colnames(new.dbs)[3:ncol(new.dbs)] <-
+      paste(colnames(new.dbs)[3:ncol(new.dbs)], "ad", sep = "_")
+    new.dbs$source.was.SBS <- TRUE
+
+    j1 <- dplyr::full_join(dbs.vcf, new.dbs)
+
+    j1[is.na(j1$source.was.SBS), "source.was.SBS"] <- FALSE
+
+  return(invisible(j1))
 
 
 }
@@ -41,7 +52,7 @@ if (FALSE) {
   debug(prep_DBS_and_adjacent_SBS)
   # 717 DBS in original inpout
   # 8 more from adjacent SBSs
-  prep_DBS_and_adjacent_SBS("~/mvv/HMF-VCFs/CPCT02020306T.purple.somatic.vcf.gz")
+  foo <- prep_DBS_and_adjacent_SBS("~/mvv/HMF-VCFs/CPCT02020306T.purple.somatic.vcf.gz")
 
 
   }
