1519d6716601545a4e6292cd9a275359b865f68c
Steven G. Rozen <steve@steverozen.net>
2021-07-14 19:07:58 +0800

    restarting the generation of Collaboratory BED and VCF files

--- data-raw/PCAWG_prep_all_Collaboratory_VCFs_and_BEDs_script.R
+++ data-raw/PCAWG_prep_all_Collaboratory_VCFs_and_BEDs_script.R
@@ -1,4 +1,4 @@
 #!/usr/local/bin/Rscript
 
 DBSverify:::PCAWG_prep_all_Collaboratory_VCFs_and_BEDs(
-  tt = "collaboratory_bams_2021_07_13_part3.csv")
+  tt = "collaboratory_bams_2021_07_13_part4.csv")
