d9251100b1dd989ce37b606c294e2fc30ce2829c
Steven G. Rozen <steve@steverozen.net>
2021-07-14 15:51:13 +0800

    restarted generating all collaboratory VCFs and BEDs after error

--- data-raw/PCAWG_prep_all_Collaboratory_VCFs_and_BEDs_script.R
+++ data-raw/PCAWG_prep_all_Collaboratory_VCFs_and_BEDs_script.R
@@ -1,3 +1,4 @@
 #!/usr/local/bin/Rscript
 
-DBSverify:::PCAWG_prep_all_Collaboratory_VCFs_and_BEDs()
+DBSverify:::PCAWG_prep_all_Collaboratory_VCFs_and_BEDs(
+  tt = "collaboratory_bams_2021_07_13_part2.csv")
