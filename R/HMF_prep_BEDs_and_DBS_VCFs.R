bd22dbf6c3681ef1bca140ba8cbb17a075b31034
Steven G. Rozen <steve@steverozen.net>
2021-07-17 15:25:53 +0800

    updated documentation

--- R/HMF_prep_BEDs_and_DBS_VCFs.R
+++ R/HMF_prep_BEDs_and_DBS_VCFs.R
@@ -1,6 +1,9 @@
 if (FALSE) {
   foo <- HMF_prep_BEDs_and_DBS_VCFs(input.vcf.dir = "~/mvv/HMF-VCFs/")
 
+  # This was the command used to generate the DBS-only VCF
+  # and BED files for Hartwig Medication Foundation (HMF)
+  # data.
   foo <- HMF_prep_BEDs_and_DBS_VCFs("~/mvv/HMF_all_input_VCF/",
                                     out.bed.dir = "~/mvv/HMF_all_BED_and_VCF",
                                     out.vcf.dir = "~/mvv/HMF_all_BED_and_VCF")
