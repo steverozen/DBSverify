580ad1725ec724e22b0ffd514ec710b340ac60c1
Steven G. Rozen <steve@steverozen.net>
2021-07-28 22:01:34 +0800

    updated a few more tests

--- tests/testthat/test_test_create_IGV_snapshot_script.R
+++ tests/testthat/test_test_create_IGV_snapshot_script.R
@@ -4,6 +4,9 @@ test_that("Is the IGV script ok", {
     vcf.name = "input/HepG2_AA1_DBS_evaluated.vcf")
   xx <- readLines(con = "input/test_IGV_script.txt")
   # Some lines point to files that have locations that vary:
-  lines.to.check <- c(1:2,4,5,7,9:4462)
+  diffs <- which(xx != ss)
+  if (length(diffs) > 0) cat("\n\ndiffs ", paste(diffs, collapse = ", "))
+  else cat("\n\nno diffs")
+  lines.to.check <- c(1:2,4:6,8,10:4462)
   expect_equal(xx[lines.to.check], ss[lines.to.check])
 })
