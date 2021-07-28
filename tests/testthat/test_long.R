887a4401e4221f22a2462362b80eba8ecde71fea
Steven G. Rozen <steve@steverozen.net>
2021-07-28 21:32:03 +0800

    stop printing timing information

--- tests/testthat/test_long.R
+++ tests/testthat/test_long.R
@@ -1,5 +1,5 @@
 test_that("Long test of Read_SBS_VCF_and_BAMs_to_verify_DBSs on input/HepG2_AA1.vcf -- full set of 636 DBSs", {
-  skip_if(Sys.getenv("DO_LONG_TEST") == "")
+  skip_if(Sys.getenv("DO_LONG_TEST") != "y")
   Sys.time()
   stime <- system.time(
     xx <- Read_SBS_VCF_and_BAMs_to_verify_DBSs(
@@ -13,9 +13,9 @@ test_that("Long test of Read_SBS_VCF_and_BAMs_to_verify_DBSs on input/HepG2_AA1.
       unlink.slice.dir = FALSE)
   )
   Sys.time()
-  cat("\nLong test timing:\n", names(stime), "\n", stime, "\n")
-  cat(round(sum(stime[1:2]) / nrow(xx$evaluated.vcf), digits = 3),
-      "CPU seconds per DBS\n")
+  # cat("\nLong test timing:\n", names(stime), "\n", stime, "\n")
+  # cat(round(sum(stime[1:2]) / nrow(xx$evaluated.vcf), digits = 3),
+  #     "CPU seconds per DBS\n")
 
   new <- data.table::fread(xx$evaluated.vcf.name)
   old <- data.table::fread(paste0(xx$evaluated.vcf.name, ".regress"))
