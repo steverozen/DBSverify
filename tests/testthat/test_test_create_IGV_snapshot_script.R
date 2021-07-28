b4e4a06737ab2f20a04ae00423fe64a878ff6f58
Steven G. Rozen <steve@steverozen.net>
2021-07-28 21:32:38 +0800

    Updated because several input files get moved in testthat

--- tests/testthat/test_test_create_IGV_snapshot_script.R
+++ tests/testthat/test_test_create_IGV_snapshot_script.R
@@ -1,6 +1,9 @@
 test_that("Is the IGV script ok", {
-  ss <- test_create_IGV_snapshot_script(run.IGV = FALSE,
-                                        vcf.name = "input/HepG2_AA1_DBS_evaluated.vcf")
+  ss <- test_create_IGV_snapshot_script(
+    run.IGV = FALSE,
+    vcf.name = "input/HepG2_AA1_DBS_evaluated.vcf")
   xx <- readLines(con = "input/test_IGV_script.txt")
-  expect_equal(xx[c(1:2,4:4462)], ss[c(1:2,4:4462)]) # Line 3 is a tempfile().
+  # Some lines point to files that have locations that vary:
+  lines.to.check <- c(1:2,4,5,7,9:4462)
+  expect_equal(xx[lines.to.check], ss[lines.to.check])
 })
