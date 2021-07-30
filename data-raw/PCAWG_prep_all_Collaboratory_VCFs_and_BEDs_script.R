84697f8471bb9f2b6dcda51d0b23a56fb1592000
Steven G. Rozen <steve@steverozen.net>
2021-07-30 08:43:02 +0800

    cleanup data-raw

--- data-raw/PCAWG_prep_all_Collaboratory_VCFs_and_BEDs_script.R
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/Rscript
-
-DBSverify:::PCAWG_prep_all_Collaboratory_VCFs_and_BEDs(
-  tt = "collaboratory_bams_2021_07_13_part4.csv")
---
+++ data-raw/production_scripts/PCAWG_prep_all_Collaboratory_VCFs_and_BEDs_script.R
@@ -0,0 +1,8 @@
+#!/usr/local/bin/Rscript
+
+# Note that I had to restart several times after encountering
+# errors. See also the input .csv files
+# collaboratory_bams_2021_07_13_part*
+
+DBSverify:::PCAWG_prep_all_Collaboratory_VCFs_and_BEDs(
+  tt = "collaboratory_bams_2021_07_13_part4.csv")
