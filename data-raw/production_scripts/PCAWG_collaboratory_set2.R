f5b61b054e1b23e783efa0e741f1b64ee195d5d3
Steven G. Rozen <steve@steverozen.net>
2021-08-05 10:46:59 +0800

    some script renaming

--- data-raw/production_scripts/PCAWG_collaboratory_set2.R
+++ data-raw/production_scripts/PCAWG_collaboratory_set2.R
@@ -5,5 +5,8 @@ library(DBSverify)
 PCAWG_read_table_and_evaluate_DBS(
   in.table = "~/DBSverify/data-raw/production_scripts/collaboratory_bams_2021_07_16.csv",
   in.vcf.dir  = "~/mvv/PCAWG_all_Collaboratory_VCFs_and_BEDs/",
-  minibam.dir = "~/mvv/collab-minibams-set1/",
-  out.vcf.dir = "~/mvv/collab-minibams-set1-output-VCFs/")
+  minibam.dir = "~/mvv/collab-minibams-set2/",
+  out.vcf.dir = "~/mvv/collab-minibams-set2-output-VCFs/")
+
+# Just in case options(warn = 1) didn't work
+warnings()
