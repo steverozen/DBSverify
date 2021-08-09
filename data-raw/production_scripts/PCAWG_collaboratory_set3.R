8dd5d9438426b1e2e27329f85d7835519316adfc
Steven G. Rozen <steve@steverozen.net>
2021-08-09 10:00:47 +0800

    added script to read VCFs and BAM slides for PCAWG Collaboratory Set 3

---
+++ data-raw/production_scripts/PCAWG_collaboratory_set3.R
@@ -0,0 +1,12 @@
+#!/usr/local/bin/Rscript
+
+library(DBSverify)
+
+PCAWG_read_table_and_evaluate_DBS(
+  in.table = "~/DBSverify/data-raw/production_scripts/collaboratory_bams_2021_07_16.csv",
+  in.vcf.dir  = "~/mvv/PCAWG_all_Collaboratory_VCFs_and_BEDs/",
+  minibam.dir = "~/mvv/collab-minibams-set3/",
+  out.vcf.dir = "~/mvv/collab-minibams-set3-output-VCFs/")
+
+# Just in case options(warn = 1) didn't work
+warnings()
