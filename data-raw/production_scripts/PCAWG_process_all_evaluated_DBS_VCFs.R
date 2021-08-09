d1771c6745c7a04d9c081f9edb349619d420f1db
Steven G. Rozen <steve@steverozen.net>
2021-08-09 10:00:12 +0800

    added script to do final processing on all evaluated PCAWG DBS VCF directories

---
+++ data-raw/production_scripts/PCAWG_process_all_evaluated_DBS_VCFs.R
@@ -0,0 +1,3 @@
+PCAWG_catalogs_from_one_VCF_directory(vcf.dir = "/home/gmssgr/mvv/collab-minibams-set1-output-VCFs-bis3/")
+
+PCAWG_catalogs_from_one_VCF_directory(vcf.dir = "/home/gmssgr/mvv/collab-minibams-set2-output-VCFs")
